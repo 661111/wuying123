d1497c742082161d9bcdbe33df7bee094d468d23
wuyin <108344166+661111@users.noreply.github.com>
2024-07-23 15:09:33 +0800

    Update sw-rules.js

--- sw-rules.js
+++ sw-rules.js
@@ -1,9 +1,18 @@
-  if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-          navigator.serviceWorker.register('/sw.js').then(function (registration) {
-              console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }).catch(function (err) {
-              console.log('ServiceWorker registration failed: ', err);
-          });
+window.addEventListener("load", event => {
+  // 判断浏览器是否支持
+  if ("serviceWorker" in navigator) {
+    console.log("支持");
+    window.navigator.serviceWorker
+      .register("/sw.js", {
+        scope: "/"
+      })
+      .then(registration => {
+        console.log("注册成功");
+      })
+      .catch(error => {
+        console.log("注册失败", error.message);
       });
+  } else {
+    console.log("不支持");
   }
+});
