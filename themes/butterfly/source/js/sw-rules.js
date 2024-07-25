a529253c67c1c7c2d04fe1ae960ffb2f313e0caf
wuyin <108344166+661111@users.noreply.github.com>
2024-07-25 13:51:58 +0800

    Update sw-rules.js

--- themes/butterfly/source/js/sw-rules.js
+++ themes/butterfly/source/js/sw-rules.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker
-            .register("/service-worker.js")
+            .register("/sw.js")
             .then((reg) => console.log("Service worker has been registered."))
             .catch((err) =>
                 console.error(`Error during service worker registration:${err}`)
