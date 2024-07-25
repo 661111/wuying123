63261d3d5c37caf318fc5d9cf91d1fa115794a5e
wuyin <108344166+661111@users.noreply.github.com>
2024-07-25 20:50:00 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,76 @@
-//用到的各个插件
-var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-html-minifier-terser');
-var htmlclean = require('gulp-htmlclean');
-var fontmin = require('gulp-fontmin');
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-html-minifier-terser');
+const htmlclean = require('gulp-htmlclean');
+const workbox = require("workbox-build");
 // gulp-tester
-var terser = require('gulp-terser');
-// 压缩js
-gulp.task('compress', async() =>{
+const terser = require('gulp-terser');
+// babel
+// const uglify = require('gulp-uglify');
+// const babel = require('gulp-babel');
+
+//pwa
+gulp.task('generate-service-worker', () => {
+  return workbox.injectManifest({
+    swSrc: './sw-template.js',
+    swDest: './public/sw.js',
+    globDirectory: './public',
+    globPatterns: [
+        "**/*.{html,css,js,json,woff2,ttf,xml}"
+    ],
+    modifyURLPrefix: {
+        "": "./"
+    }
+  });
+});
+
+//minify js babel
+// gulp.task('compress', async() =>
+//   gulp.src(['./public/**/*.js', '!./public/**/*.min.js','!./public/js/custom/galmenu.js','!./public/js/custom/gitcalendar.js','!./public/js/custom/runtime.js','!./public/js/custom/wow_init.js','!./public/live2d_widget/waifu_init.js','!./public/live2d_widget/waifu-tips.js'])
+// 		.pipe(babel({
+// 			presets: ['@babel/preset-env']
+// 		}))
+//     .pipe(uglify().on('error', function(e){
+//       console.log(e);
+//     }))
+// 		.pipe(gulp.dest('./public'))
+// });
+// minify js - gulp-tester
+gulp.task('compress', async() =>
   gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
     .pipe(terser())
     .pipe(gulp.dest('./public'))
 });
-//压缩css
-gulp.task('minify-css', () => {
-    return gulp.src(['./public/**/*.css'])
-        .pipe(cleanCSS({
-            compatibility: 'ie11'
-        }))
-        .pipe(gulp.dest('./public'));
-});
-//压缩html
-gulp.task('minify-html', () => {
-    return gulp.src('./public/**/*.html')
-        .pipe(htmlclean())
-        .pipe(htmlmin({
-            removeComments: true, //清除html注释
-            collapseWhitespace: true, //压缩html
-            collapseBooleanAttributes: true,
-            //省略布尔属性的值，例如：<input checked="true"/> ==> <input />
-            removeEmptyAttributes: true,
-            //删除所有空格作属性值，例如：<input id="" /> ==> <input />
-            removeScriptTypeAttributes: true,
-            //删除<script>的type="text/javascript"
-            removeStyleLinkTypeAttributes: true,
-            //删除<style>和<link>的 type="text/css"
-            minifyJS: true, //压缩页面 JS
-            minifyCSS: true, //压缩页面 CSS
-            minifyURLs: true  //压缩页面URL
-        }))
-        .pipe(gulp.dest('./public'))
-});
-//压缩字体
-function minifyFont(text, cb) {
-  gulp
-    .src('./public/fonts/*.ttf') //原字体所在目录
-    .pipe(fontmin({
-      text: text
-    }))
-    .pipe(gulp.dest('./public/fontsdest/')) //压缩后的输出目录
-    .on('end', cb);
-}
 
-gulp.task('mini-font', (cb) => {
-  var buffers = [];
-  gulp
-    .src(['./public/**/*.html']) //HTML文件所在目录请根据自身情况修改
-    .on('data', function(file) {
-      buffers.push(file.contents);
-    })
-    .on('end', function() {
-      var text = Buffer.concat(buffers).toString('utf-8');
-      minifyFont(text, cb);
-    });
+//css
+gulp.task('minify-css', () => {
+  return gulp.src('./public/**/*.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie11'
+    }))
+    .pipe(gulp.dest('./public'));
 });
-// 运行gulp命令时依次执行以下任务
-gulp.task('default', gulp.parallel(
-  'compress', 'minify-css', 'minify-html','mini-font'
-))
+
+
+// 壓縮 public 目錄內 html
+gulp.task('minify-html', () => {
+  return gulp.src('./public/**/*.html')
+    .pipe(htmlclean())
+    .pipe(htmlmin({
+      removeComments: true, //清除 HTML 註釋
+      collapseWhitespace: true, //壓縮 HTML
+      collapseBooleanAttributes: true, //省略布爾屬性的值 <input checked="true"/> ==> <input />
+      removeEmptyAttributes: true, //刪除所有空格作屬性值 <input id="" /> ==> <input />
+      removeScriptTypeAttributes: true, //刪除 <script> 的 type="text/javascript"
+      removeStyleLinkTypeAttributes: true, //刪除 <style> 和 <link> 的 type="text/css"
+      minifyJS: true, //壓縮頁面 JS
+      minifyCSS: true, //壓縮頁面 CSS
+      minifyURLs: true
+    }))
+    .pipe(gulp.dest('./public'))
+});
+
+// 執行 gulp 命令時執行的任務
+gulp.task("default", gulp.series("generate-service-worker", gulp.parallel(
+    'compress','minify-html', 'minify-css'
+)));
